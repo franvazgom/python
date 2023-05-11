6048b3fa954142d5f6ca5995a7560f9362f3e798
Francisco Vazquez <franvazgom@gmail.com>
2023-05-10 21:05:26 -0600

    practicas del 10 mayo

---
+++ code/Modulo-I/PracticasClase/practica_input.py
@@ -0,0 +1,13 @@
+# Pedir un numero de teclado
+# imprimir el resultado de ese numero multiplicado por 8 
+
+# print("Teclea un numero: ")
+# x = input() # entra una cadena (string)
+# x = int(x)
+# res = x * 8
+# # print("El resultado de tu numero * 8 = ", res, ' Gracias!!!')
+# print("El resultado de tu numero * 8 = " + str(res) + ' Gracias!!!')
+
+
+x = int(input('Teclea un numero: '))
+print("El resultado de tu numero * 8 = " + str(x * 8) + ' Gracias!!!')
---
+++ code/Modulo-I/PracticasClase/practica_input_main.py
@@ -0,0 +1,10 @@
+
+def main():
+    x = int(input('Teclea un numero: '))
+    print("El resultado de tu numero * 8 = ", x*8)
+
+if __name__ == '__main__':
+    main()
+
+
+
