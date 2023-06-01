63325d8ad8d4b9ee6c48a6b6ba9e6f976cb7b2c1
franvazgomdev <franvazgomdev@gmail.com>
2023-05-31 20:44:22 -0600

    grafias y tablas

--- code/Modulo-I/06_archivos_graficas/assignments/02_EscrituraArchivo/src/exercise.py
+++ code/Modulo-I/06_archivos_graficas/assignments/02_EscrituraArchivo/src/exercise.py
@@ -6,10 +6,16 @@ lines = ['linea 1', 'Línea 2', 'linea 3']
             f.write(line)
             f.write('\n')
 '''
-
+import os
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    path = os.path.dirname(__file__)
+    lines = ['frase 1 otras cosas.. comunicación', 'Segunda frase..', 'frase numero 3']
+    with open(path + '/../../../files/datos_salida.csv', 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write(line)
+            f.write('\n')
+    f.close()
 
 if __name__ == '__main__':
     main()
--- code/Modulo-I/06_archivos_graficas/assignments/04_Tablas/src/exercise.py
+++ code/Modulo-I/06_archivos_graficas/assignments/04_Tablas/src/exercise.py
@@ -11,10 +11,20 @@ una lista con los titulos de las columnas
     axis('tight')
     axis('off')
 '''
+from matplotlib import pyplot as plt
+
+def muestra_tabla(datos):
+    fig, ax = plt.subplots(figsize=(6,3))
+    column_labels = ['Estado', 'Calificación']
+    ax.table(cellText=datos, colLabels=column_labels, loc="center")
+    ax.axis('tight')
+    ax.axis('off')
+    plt.show()
 
 def main():
     # Escribe tu código abajo de esta línea
-    pass
+    matriz = [['Aguascalientes', 98], ['Ciudad de México', 34], ['Puebla', 56]]
+    muestra_tabla(matriz)
 
 if __name__ == '__main__':
     main()
