1d3e445d0a13858434a2fc7ce8d7e85b2bcf1c10
franvazgomdev <franvazgom@gmnail.com>
2023-12-13 21:03:48 -0600

    Clase 13/12/23

--- code/Modulo-VII/PracticaDjangoRest/djangoRest/projects/tests.py
+++ code/Modulo-VII/PracticaDjangoRest/djangoRest/projects/tests.py
@@ -1,3 +1,71 @@
-from django.test import TestCase
+# from django.test import TestCase
+from rest_framework.test import APITestCase
+import json
+from projects.models import Project
 
-# Create your tests here.
+
+class BasicTest(APITestCase):
+    def setUp(self):
+        self.url = '/apiTest/'
+    
+    def test_get(self):
+        response = self.client.get(self.url, format='json')
+        self.assertEquals(response.status_code, 200)
+
+class ProjectServiceTest(APITestCase):
+    def setUp(self):
+        self.url = '/apiServices/'
+        for i in range(10):
+            Project.objects.create(
+                title='Proyecto ' + str(i),
+                description='description ' + str(i),
+                quantity=i*3) 
+    
+    def test_with_out_parameters(self):
+        data = {
+            'parameters':{}
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+        self.assertEquals(response.status_code, 200)
+        data_res = json.loads(response.data['Resultado'])
+        self.assertEquals(len(data_res), 10)
+        self.assertEquals(data_res[0]['title'], "Proyecto 0")
+
+    def test_with_title_parameter(self):
+        data = {
+            'parameters':{'title':'Proyecto 1'}
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+        self.assertEquals(response.status_code, 200)
+        data_res = json.loads(response.data['Resultado'])
+        self.assertEquals(len(data_res), 1)
+        self.assertEquals(data_res[0]['title'], "Proyecto 1")
+    
+    def test_with_description_parameter(self):
+        data = {
+            'parameters':{'description':'5'}
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+        self.assertEquals(response.status_code, 200)
+        data_res = json.loads(response.data['Resultado'])
+        self.assertEquals(len(data_res), 1)
+        self.assertEquals(data_res[0]['title'], "Proyecto 5")
+
+    def test_with_two_parameters(self):
+        data = {
+            'parameters':{'description':'desc', 'quantity':12}
+        }
+
+        response = self.client.post(self.url, data=data, format='json')
+        self.assertEquals(response.status_code, 200)
+        data_res = json.loads(response.data['Resultado'])
+        self.assertEquals(len(data_res), 5)
+        self.assertEquals(data_res[-1]['title'], "Proyecto 9")
+
+
+
+
+
--- code/Modulo-VII/webrestaurante/webrestaurante/settings.py
+++ code/Modulo-VII/webrestaurante/webrestaurante/settings.py
@@ -82,10 +82,21 @@ WSGI_APPLICATION = 'webrestaurante.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "restaurante",
+        "USER": "user_db",
+        "PASSWORD": "hola1234",
+        "HOST": "127.0.0.1",
+        "PORT": "3306",
     }
 }
 
